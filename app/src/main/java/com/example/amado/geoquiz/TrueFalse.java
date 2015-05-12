7f47904a478b1a30fec8c83e93a1f1a355c3db65
RoyMontoya <roy_a18@hotmail.com>
2015-05-11 16:12:38 -1000

    github backup

---
+++ app/src/main/java/com/example/amado/geoquiz/TrueFalse.java
@@ -0,0 +1,33 @@
+package com.example.amado.geoquiz;
+
+/**
+ * Created by Amado on 11/05/2015.
+ */
+public class TrueFalse {
+
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+
+    public TrueFalse(int question, boolean trueQuestion){
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+
+
+    }
+}
